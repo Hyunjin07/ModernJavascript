b3171f630665c80e423ef10c62bd0bd93a95e910
ca970 <ca5522397@gmail.com>
2024-05-26 20:55:48 -0400

    updated variable.js

--- variable.js
+++ variable.js
@@ -1,26 +1,35 @@
-var nan = "난" / 10;
-console.log(nan);
+// var nan = "난" / 10;
+// console.log(nan);
 
-var lang = "javascript";
-var love = "사랑하";
-var you = null;
+// var lang = "javascript";
+// var love = "사랑하";
+// var you = null;
 
-var num = 5;
-console.log(lang + null + love + num);
+// var num = 5;
+// console.log(lang + null + love + num);
 
-var msg =
-  "프레임워크의 일부를 사용하는 것부터 사용자 지정 컴포넌트 및 레이아웃에 이르기까지 다양한 예시를 통해 프로젝트를 빠르게 시작하세요.";
-console.log(msg.length);
+// var msg =
+//   "프레임워크의 일부를 사용하는 것부터 사용자 지정 컴포넌트 및 레이아웃에 이르기까지 다양한 예시를 통해 프로젝트를 빠르게 시작하세요.";
+// console.log(msg.length);
 
-var msg1 = 'She said "Good Bye"';
+// var msg1 = 'She said "Good Bye"';
 
-var msg2 = "she's gone";
+// var msg2 = "she's gone";
 
-console.log(msg1);
-console.log(msg2);
+// console.log(msg1);
+// console.log(msg2);
 
-var bigger = 100 > 50;
-console.log(bigger);
+// var bigger = 100 > 50;
+// console.log(bigger);
 
-bigger = 100 < 50;
-console.log(bigger);
+// bigger = 100 < 50;
+// console.log(bigger);
+
+function myTest() {
+  if (true) {
+    const x = { name: "kim", age: 20 }; // 주소값이 cost가 되었기 때문에 안전성 보장
+    x = 200;
+  }
+  console.log(x);
+}
+myTest();
